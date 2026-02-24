1941b21a01af69c8aa9d00af91509455c282bace
akhil0703-gif <akhilkumar0703@gmail.com>
2026-02-24 16:18:01 +0530

    Update output.tf

--- project/output.tf
+++ project/output.tf
@@ -1,8 +1,8 @@
-output "vpc_id" {
-  value = google_compute_network.custom_vpc.id
-}
+#output "vpc_id" {
+  #value = google_compute_network.custom_vpc.id
+#}
 
-output "standalone_vm_public_ip" {
-  description = "The public IP address of the VM on the default network"
-  value       = google_compute_instance.test_vm.network_interface[0].access_config[0].nat_ip
-}
+#output "standalone_vm_public_ip" {
+  #description = "The public IP address of the VM on the default network"
+  #value       = google_compute_instance.test_vm.network_interface[0].access_config[0].nat_ip
+#}
