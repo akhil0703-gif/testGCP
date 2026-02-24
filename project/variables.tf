9b5aaad7eb08128e539389ff71951c21bbc98258
akhil0703-gif <akhilkumar0703@gmail.com>
2026-02-24 11:50:42 +0530

    Update variables.tf

--- project/variables.tf
+++ project/variables.tf
@@ -7,6 +7,18 @@ variable "gcp_region" {
   default = "us-central1"
 }
 
+variable "zone" {
+  description = "The GCP zone for the standalone VM"
+  type        = string
+  default     = "asia-south1-a" 
+}
+
 variable "vpc_name" {
   type = string
+}
+
+variable "machine_type" {
+  description = "The machine type for the VM"
+  type        = string
+  default     = "e2-micro"
 }
